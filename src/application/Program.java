60709ac7f05468646f616de1958fc69e5fa40775
Tupi Guedes <tupiguedes@outlook.com>
2021-08-21 14:35:38 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -14,6 +16,9 @@ public class Program {
 
 		Seller seller = new Seller(1, "Tupi Guedes Ribas", "tupyribas@outlook.com", sdf.parse("21/10/2002"), 1200.0,
 				obj);
+		
+		SellerDAO sellerDAO = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.DepartmentDAO;
+import model.entities.Department;
+
+public class DepartmentDaoJDBC implements DepartmentDAO {
+
+	@Override
+	public void insert(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Department obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Department findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Department> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
