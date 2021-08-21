fd918a3221ad43ce0ac1619b18bf29ea455adeb5
Tupi Guedes <tupiguedes@outlook.com>
2021-08-21 12:07:19 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Department obj = new Department(1, "Bocks");
-		System.out.println(obj);
+
+		Seller seller = new Seller(1, "Tupi Guedes Ribas", "tupyribas@outlook.com", sdf.parse("21/10/2002"), 1200.0,
+				obj);
+		System.out.println(seller);
 	}
 
 }
