3da93a9ce1c516a76e05c353df665e10eba8e838
Tupi Guedes <tupiguedes@outlook.com>
2021-08-22 20:14:25 -0300

    findById implementation and and method fixes

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDAO;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		
+		DepartmentDAO departmentDAO = DaoFactory.createDepartmentDao();
+		Department dep = new Department();
 
+		System.out.println("=====TESTE 01===== \nMostrar dados do departamento específico");
+		dep = departmentDAO.findById(3);
+		System.out.println(dep);
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,13 @@
 package model.dao.impl;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDAO;
 import model.entities.Department;
 
@@ -34,9 +39,35 @@ public class DepartmentDaoJDBC implements DepartmentDAO {
 
 	@Override
 	public Department findById(Integer id) {
-
-		return null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			String sql = "SELECT * FROM department WHERE Id = ?";
+			stmt = conn.prepareStatement(sql);
+			
+			stmt.setInt(1, id);
+			
+			rs = stmt.executeQuery();
+			
+			if (rs.next()) {
+				return instantiateDepartment(rs);
+			}
+			return null;
+		} 
+		catch (SQLException e) {
+			throw new DbException("FAILED TO SHOW DEPARTMENT DATA cod.:012>>> " + e.getMessage());
+		} 
+		finally {
+			DB.closeConnection(stmt, rs);
+		}
 	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("Name"));
+		return dep;
+	} 
 
 	@Override
 	public List<Department> findAll() {
